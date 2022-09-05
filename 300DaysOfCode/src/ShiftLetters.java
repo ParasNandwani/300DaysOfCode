79a31bda0f7dd41fd89c1dd48ea2885e478e6809
PARAS NANDWANI <parasnandwani10@gmail.com>
2022-09-05 21:36:15 +0530

    code changes

--- 300DaysOfCode/src/ShiftLetters.java
+++ 300DaysOfCode/src/ShiftLetters.java
@@ -3,24 +3,27 @@ import org.omg.CORBA.StringHolder;
 public class ShiftLetters {
 
     public static void main(String[] args) {
-      String  s = "abc";
-      int[] shifts = new int[]{3,5,9};
-      System.out.println(shiftStr(s,shifts));
+        String s = "abc";
+        int[] shifts = new int[]{3, 5, 9};
+        System.out.println(shiftingLetters(s, shifts));
     }
 
-    public static String shiftStr(String s, int[] shifts) {
-        int sum  = 0;
-        for(int i = 0 ; i < shifts.length ; i++) {
+
+    public static String shiftingLetters(String s, int[] shifts) {
+        int sum = 0;
+        for (int i = 0; i < shifts.length; i++) {
             sum += shifts[i];
         }
         StringBuilder sBuilder = new StringBuilder();
         sBuilder.append("");
-        for(int i = 0 ; i < s.length(); i++) {
-            char c = (char) (s.charAt(i)+sum);
+        for (int i = 0; i < s.length(); i++) {
+            int sumAscii = sum % 26;
+            char c = (char) (((s.charAt(i) - 'a') + sumAscii) % 26 + 'a');
             sBuilder.append(c);
             sum -= shifts[i];
         }
-
         return sBuilder.toString();
     }
+
 }
+
